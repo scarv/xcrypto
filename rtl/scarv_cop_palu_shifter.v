b9f4d602a0c1d411d155a93ca48b1eefe33d2dc9
Ben Marshall <ben.marshall@bristol.ac.uk>
2018-09-24 09:51:43 +0100

    Packed shifter model implemented with updated rot.px behaviour
    Implemented the shift and rotate packed instructions.
    
    - Closes #19 - rot.px specification update.
    - Unblocks #18 - mul.px broken due to lack of correct shift behaviour.
    - Only mul.px left to do for issue #11
    
     On branch dev/ben-marshall
     Changes to be committed:
    	modified:   docs/crypto-ise.specification.tex
    	modified:   rtl/scarv_cop_palu_shifter.v
    	modified:   verif/model/model_ise.v

--- rtl/scarv_cop_palu_shifter.v
+++ rtl/scarv_cop_palu_shifter.v
@@ -32,51 +32,99 @@ wire   pw_16         = pw == SCARV_COP_PW_16; // 16  2-bit crumbs
 
 wire [63:0] shin = {r ? a : 32'b0,  a};
 
-wire [32:0]   pw_mask       = {
-    {16{pw_1                                 }} , 
-    { 8{pw_2                          || pw_1}} ,
-    { 4{pw_4                  || pw_2 || pw_1}} ,
-    { 2{pw_8          || pw_4 || pw_2 || pw_1}} ,
-    { 1{pw_16 || pw_8 || pw_4 || pw_2 || pw_1}} ,
-    { 1{1'b1                                 }} 
+wire [31:0] result_1 ;
+wire [31:0] result_2 ;
+wire [31:0] result_4 ;
+wire [31:0] result_8 ;
+wire [31:0] result_16;
+
+assign c = 
+    {32{pw_1 }}  & result_1  |
+    {32{pw_2 }}  & result_2  |
+    {32{pw_4 }}  & result_4  |
+    {32{pw_8 }}  & result_8  |
+    {32{pw_16}}  & result_16 ;
+
+assign result_1 = sl ? a << shamt : shin >> shamt;
+
+`define PACKSHIFT(HI,LO, WIDTH) sl ? a[HI:LO] << shamt : \
+                            {r ? shin[HI:LO] : WIDTH'b0, shin[HI:LO]} >> shamt
+
+wire [31:0] result_2_0   = `PACKSHIFT(31, 16, 16);
+wire [31:0] result_2_1   = `PACKSHIFT(15,  0, 16);
+
+wire [15:0] result_4_0   = `PACKSHIFT(31, 24, 8 );
+wire [15:0] result_4_1   = `PACKSHIFT(23, 16, 8 );
+wire [15:0] result_4_2   = `PACKSHIFT(15,  8, 8 );
+wire [15:0] result_4_3   = `PACKSHIFT( 7,  0, 8 );
+
+wire [ 7:0] result_8_0   = `PACKSHIFT(31, 28, 4 );
+wire [ 7:0] result_8_1   = `PACKSHIFT(27, 24, 4 );
+wire [ 7:0] result_8_2   = `PACKSHIFT(23, 20, 4 );
+wire [ 7:0] result_8_3   = `PACKSHIFT(19, 16, 4 );
+wire [ 7:0] result_8_4   = `PACKSHIFT(15, 12, 4 );
+wire [ 7:0] result_8_5   = `PACKSHIFT(11,  8, 4 );
+wire [ 7:0] result_8_6   = `PACKSHIFT( 7,  4, 4 );
+wire [ 7:0] result_8_7   = `PACKSHIFT( 3,  0, 4 );
+
+wire [ 3:0] result_16_0  = `PACKSHIFT(31, 30, 2 );
+wire [ 3:0] result_16_1  = `PACKSHIFT(29, 28, 2 );
+wire [ 3:0] result_16_2  = `PACKSHIFT(27, 26, 2 );
+wire [ 3:0] result_16_3  = `PACKSHIFT(25, 24, 2 );
+wire [ 3:0] result_16_4  = `PACKSHIFT(23, 22, 2 );
+wire [ 3:0] result_16_5  = `PACKSHIFT(21, 20, 2 );
+wire [ 3:0] result_16_6  = `PACKSHIFT(19, 18, 2 );
+wire [ 3:0] result_16_7  = `PACKSHIFT(17, 16, 2 );
+wire [ 3:0] result_16_8  = `PACKSHIFT(15, 14, 2 );
+wire [ 3:0] result_16_9  = `PACKSHIFT(13, 12, 2 );
+wire [ 3:0] result_16_10 = `PACKSHIFT(11, 10, 2 );
+wire [ 3:0] result_16_11 = `PACKSHIFT( 9,  8, 2 );
+wire [ 3:0] result_16_12 = `PACKSHIFT( 7,  6, 2 );
+wire [ 3:0] result_16_13 = `PACKSHIFT( 5,  4, 2 );
+wire [ 3:0] result_16_14 = `PACKSHIFT( 3,  2, 2 );
+wire [ 3:0] result_16_15 = `PACKSHIFT( 1,  0, 2 );
+
+assign result_2 = {
+result_2_1[15:0],
+result_2_0[15:0] 
+};                                           
+                                             
+assign result_4 = {                          
+result_4_3[7:0],
+result_4_2[7:0],
+result_4_1[7:0],
+result_4_0[7:0] 
+};                                           
+                                             
+assign result_8 = {                          
+result_8_7[3:0],
+result_8_6[3:0],
+result_8_5[3:0],
+result_8_4[3:0],
+result_8_3[3:0],
+result_8_2[3:0],
+result_8_1[3:0],
+result_8_0[3:0] 
+};                                           
+                                             
+assign result_16 = {                         
+result_16_15[1:0],
+result_16_14[1:0],
+result_16_13[1:0],
+result_16_12[1:0],
+result_16_11[1:0],
+result_16_10[1:0],
+result_16_9 [1:0],
+result_16_8 [1:0],
+result_16_7 [1:0],
+result_16_6 [1:0],
+result_16_5 [1:0],
+result_16_4 [1:0],
+result_16_3 [1:0],
+result_16_2 [1:0],
+result_16_1 [1:0],
+result_16_0 [1:0] 
 };
-
-wire [ 4:0]   ch_mask       = {
-    pw_1                                 , 
-    pw_2                          || pw_1,
-    pw_4                  || pw_2 || pw_1,
-    pw_8          || pw_4 || pw_2 || pw_1,
-    pw_16 || pw_8 || pw_4 || pw_2 || pw_1 
-};
-
-wire [5:0]   pw_1hot       = {
-    pw_1 , 
-    pw_2 ,
-    pw_4 ,
-    pw_8 ,
-    pw_16,
-    1'b0
-};
-
-wire [ 5:0] msk_shamt   = pw_mask & shamt;
-
-wire [31:0] sr_left     = shin << shamt;
-wire [31:0] sr_right    = shin >> shamt;
-
-wire [ 4:0] dst_chunks[31:0];
-wire [ 4:0] src_chunks[31:0];
-
-genvar i;
-
-generate for(i = 0; i < 32; i = i + 1) begin
-
-    assign dst_chunks[i]   = i & ~(ch_mask);
-    assign src_chunks[i]   = (sl||r ? (i-shamt) : (i+shamt)) & ~(ch_mask);
-    
-    assign c[i] = ((sl || r) ? sr_left[i] : sr_right[i]) &&
-        dst_chunks[i] == src_chunks[i];
-
-end endgenerate
 
 endmodule
 
