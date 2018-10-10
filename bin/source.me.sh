50b2178a9a6223e2f8e10f102a8743ff44fc95c7
Ben Marshall <ben.marshall@bristol.ac.uk>
2018-10-10 09:46:19 +0100

    Add YS_INSTALL to environment variables in source.me
     On branch dev/ben-marshall
     Changes to be committed:
    	modified:   bin/source.me.sh

--- bin/source.me.sh
+++ bin/source.me.sh
@@ -8,9 +8,14 @@ echo "-------------------------[Setting Up Project]--------------------------"
 export COP_HOME=`pwd`
 export COP_WORK=$COP_HOME/work
 
+if [ -z $YS_INSTALL ] ; then
+    export YS_INSTALL=
+fi
+
 mkdir -p $COP_WORK
 
 echo "COP_HOME      = $COP_HOME"
 echo "COP_WORK      = $COP_WORK"
+echo "YS_INSTALL    = $YS_INSTALL"
 
 echo "------------------------------[Finished]-------------------------------"
