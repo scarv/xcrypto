c54be09447187dcc753aa26468263cc1d7f436e4
Ben Marshall <ben.marshall@bristol.ac.uk>
2018-10-23 11:37:57 +0100

    Removing useless make target and redundant formal check.
    - Still having trouble with the xc.mmul.1 instruction proof.
    
     On branch dev/ben-marshall
     Changes to be committed:
    	modified:   flow/yosys/Makefile
    	modified:   verif/formal/fml_chk_instr_mmul_1.v
    	deleted:    verif/formal/fml_chk_instr_mmul_1_fixed_regs.v

--- verif/formal/fml_chk_instr_mmul_1.v
+++ verif/formal/fml_chk_instr_mmul_1.v
@@ -15,6 +15,9 @@
 
 reg [63:0] value;
 
+// Assume only this instruction is ever run.
+always @(posedge `VTX_CLK_NAME) if(vtx_valid) assume(dec_mmul_1);
+
 //
 // mmul_1
 //
@@ -24,7 +27,7 @@ reg [63:0] value;
 
     value = (`CRS1 * `CRS2) + `CRS3;
 
-    `VTX_ASSERT_CRDM_VALUE_IS(value)
+    `VTX_ASSERT(vtx_crd1_val_post == value[31:0]);
 
     // Never causes writeback to GPRS
     `VTX_ASSERT_WEN_IS_CLEAR
