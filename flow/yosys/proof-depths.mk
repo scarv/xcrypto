ef124acebf3fdd1c97b6ba08c00365144dfcf6c3
Ben Marshall <ben.marshall@bristol.ac.uk>
2018-11-02 09:59:48 +0000

    Formal checks for `pclmul_[l|h]`
     On branch dev/new-instructions
     Changes to be committed:
    	modified:   flow/yosys/proof-depths.mk
    	new file:   verif/formal/fml_chk_instr_pclmul_h.v
    	new file:   verif/formal/fml_chk_instr_pclmul_l.v
    	modified:   verif/formal/fml_pack_widths.vh

--- flow/yosys/proof-depths.mk
+++ flow/yosys/proof-depths.mk
@@ -7,10 +7,10 @@
 
 $(SMTDIR)/instr_pmul_h.rpt     : BMC_STEPS=20
 $(SMTDIR)/instr_pmul_h_pw2.rpt : BMC_STEPS=20
-$(SMTDIR)/instr_pmul_h_pw1.rpt : BMC_STEPS=36
+$(SMTDIR)/instr_pmul_h_pw1.rpt : BMC_STEPS=40
 
 $(SMTDIR)/instr_pmul_l.rpt     : BMC_STEPS=15
 $(SMTDIR)/instr_pmul_l_pw2.rpt : BMC_STEPS=20
-$(SMTDIR)/instr_pmul_l_pw1.rpt : BMC_STEPS=36
+$(SMTDIR)/instr_pmul_l_pw1.rpt : BMC_STEPS=40
 
 $(SMTDIR)/protocols.rpt        : BMC_STEPS=40
