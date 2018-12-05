9047375cfdf960443bf0a3e80b197e0a45e6d3c2
Ben Marshall <ben.marshall@bristol.ac.uk>
2018-12-05 16:10:25 +0000

    Add example KeccakP-400 code + bumb libscarv
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   examples/Makefile
    	new file:   examples/keccakp-400/Makefile
    	new file:   examples/keccakp-400/keccakp-400.c
    	modified:   external/libscarv

---
+++ examples/keccakp-400/keccakp-400.c
@@ -0,0 +1,70 @@
+
+#include <stdint.h>
+#include "common.h"
+
+#include "scarv/KeccakP-400-SnP.h"
+
+void rand_init_state(tKeccakLane * state) {
+    for(int i = 0; i < KeccakP400_stateSizeInBytes/2; i ++) {
+        uint32_t sample;
+        rngsamp(&sample);
+        state[i] = (tKeccakLane)sample;
+    }
+}
+
+int main() {
+  
+    tKeccakLane keccak_state1 [KeccakP400_stateSizeInBytes/2];
+    tKeccakLane keccak_state2 [KeccakP400_stateSizeInBytes/2];
+
+    rand_init_state(keccak_state1);
+
+    /*
+    for(int i = 0; i < KeccakP400_stateSizeInBytes/2; i ++) {
+        keccak_state2[i] = keccak_state1[i];
+
+        puthex((uint32_t)keccak_state1[i]);
+        putstr(", ");
+        puthex((uint32_t)keccak_state2[i]);
+        putstr("\n");
+    }
+    putstr("\n");
+    */
+    
+    uint32_t rounds      = 11;
+    uint32_t instr_start = rdinstret();
+    uint32_t cycle_start = rdcycle();
+    
+    KeccakP400_Permute_Nrounds(keccak_state1, rounds);
+
+    uint32_t instr_count = rdinstret() - instr_start;
+    uint32_t cycle_count = rdcycle()   - cycle_start;
+    
+    /*
+    for(int i = 0; i < KeccakP400_stateSizeInBytes/2; i ++) {
+        puthex(keccak_state1[i]);
+        putstr(", ");
+        puthex(keccak_state2[i]);
+
+        if(keccak_state1[i] != keccak_state2[i]) {
+            putstr(" !");
+        }
+
+        putstr("\n");
+    }
+    putstr("\n");
+    */
+
+    putstr("# Cycle Count: ");
+    puthex(cycle_count);
+    putstr("\n# Instr Count: ");
+    puthex(instr_count);
+    putstr("\n");
+    putstr("# Cycle/Round: ");
+    puthex(cycle_count/rounds);
+    putstr("\n# Instr/Round: ");
+    puthex(instr_count/rounds);
+    putstr("\n");
+
+    __pass();
+}
