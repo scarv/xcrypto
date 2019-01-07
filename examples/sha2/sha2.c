7881a879b8e3c47f3e438a4ec6f27abf83ba6a62
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-01-07 15:23:29 +0000

    SHA2 test framework

--- examples/sha2/sha2.c
+++ examples/sha2/sha2.c
@@ -11,23 +11,41 @@ int main() {
     uint8_t         input_bytes [input_len];
     uint8_t         hash        [hash_len ];
 
-    for(int i = 0; i < 10; i  ++) {
+    for(int i = 0; i < input_len; i ++) {
+        input_bytes[i] = i & 0xFF;
+    }
+
+    putstr("import hashlib\n");
+    putstr("import binascii\n");
+    putstr("import sys\n");
+
+    for(int i = 0; i < 3; i  ++) {
         
-        putstr("0x");
-        for(int j = input_len-1; j >= 0; j --) {
+        putstr("msg = '");
+        for(int j = 0; j < input_len; j ++) {
             puthex8(input_bytes[j]);
         }
-        putstr(" ");
+        putstr("'\n");
+        putstr("msg_in = binascii.a2b_hex(msg)\n");
 
         sha2_256(hash, 1, input_bytes, input_len);
         
-        putstr("0x");
-        for(int j = hash_len-1; j >= 0; j --) {
+        putstr("hash_out = '");
+        for(int j = 0; j < hash_len; j ++) {
             puthex8(hash[j]);
 
             input_bytes[j] = hash[j];
         }
-        putchar('\n');
+        putstr("'\n");
+
+        putstr("golden = hashlib.sha256(msg_in).hexdigest().upper()\n");
+        putstr("print('msg      = %s' % msg     )\n");
+        putstr("print('hash_out = %s' % hash_out)\n");
+        putstr("print('golden   = %s' % golden  )\n");
+        putstr("print('')\n");
+        putstr("if(golden != hash_out):\n");
+        putstr("    print('ERROR')\n");
+        putstr("    sys.exit(1)\n");
 
     }
 
