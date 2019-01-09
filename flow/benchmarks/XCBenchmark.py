08c249c587a2d8fa213d62b6d73acdca9fb52ace
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-01-09 14:07:59 +0000

    MPN example now working with new benchmark flow.
     On branch dev/benchmarking
     Changes to be committed:
    	modified:   .gitignore
    	new file:   examples/common/benchmark.h
    	modified:   examples/common/common.mk
    	modified:   examples/mpn/mpn.c
    	modified:   flow/benchmarks/Makefile
    	modified:   flow/benchmarks/XCBenchmark.py

--- flow/benchmarks/XCBenchmark.py
+++ flow/benchmarks/XCBenchmark.py
@@ -16,16 +16,52 @@ class BaseTester(object):
         """
         Create a new empty tester object.
         """
+    
+    def golden(self,inputs):
+        """
+        Return the golden/trusted set of outputs given the inputs
+        """
+        return []
 
-
-    def test(inputs, outputs):
+    def test(self,inputs, outputs):
         """
         Take a tuple of inputs and the expected output(s), return True or
         False if the output is correct for the given input.
         """
-        assert(type(inputs) == tuple)
-        assert(type(outputs) == tuple)
+        results = self.golden(inputs)
+
+        for i in range(0, len(outputs)):
+            if(outputs[i] != results[i]):
+                return False
         return True
+
+class MPNAddTester(BaseTester):
+    """
+    Tester class for multi-precision add operations
+    """
+
+    def golden(self,inputs):
+        lhs, rhs = inputs
+        return     [lhs + rhs]
+
+
+class MPNSubTester(BaseTester):
+    """
+    Tester class for multi-precision subtract operations
+    """
+
+    def golden(self,inputs):
+        lhs, rhs = inputs
+        return     [lhs - rhs]
+
+class MPNMulTester(BaseTester):
+    """
+    Tester class for multi-precision multiply operations
+    """
+
+    def golden(self,inputs):
+        lhs, rhs = inputs
+        return     [lhs * rhs]
 
 
 class ResultsSetRecord(object):
@@ -44,11 +80,12 @@ class ResultsSetRecord(object):
         self.correct    = correct
 
     def __repr__(self):
-        sys.stdout.write("%5s," self.correct)
-        sys.stdout.write(",".join(self.inputs))
-        sys.stdout.write(",".join(self.outputs))
+        tr  = "%5s," % self.correct
+        tr +=",".join([str(s) for s in self.inputs ])
+        tr +=",".join([str(s) for s in self.outputs])
         for k in self.metrics:
-            sys.stdout.write(",%s=%s"%(k,self.metrics[k]))
+            tr += ",%s=%s"%(k,self.metrics[k])
+        return tr
 
 
 class ResultsSet(object):
@@ -58,17 +95,24 @@ class ResultsSet(object):
     counts and correctness test results for each operation.
     """
 
-    def __init__(self, testerClass = BaseTester()):
+    def __init__(self, name, testerClass = BaseTester()):
         """
         Create a new result set with the given testerClass object for
         checking the correctness of any results
 
         :param BaseTester testerClass:
         """
-
+        
+        self.name    = name
         self.tester  = testerClass
         self.results = []
 
+    def addRecord(self,rec):
+        """
+        Add a new record to the results set.
+        """
+        rec.correct = self.tester.test(rec.inputs,rec.outputs)
+        self.results.append(rec)
 
     def addResult(inputs, outputs, metrics):
         """
@@ -81,8 +125,17 @@ class ResultsSet(object):
         record  = ResultsSetRecord(inputs,outputs,metrics,correct)
         
         self.results.append(record)
+    
+    def allCorrect(self):
+        
+        passed = True
+        for r in self.results:
+            passed = passed and r.correct
+            if(not passed):
+                break
+        return passed
 
-    def printResults():
+    def printResults(self):
         """
         Print the results set to stdout
         """
