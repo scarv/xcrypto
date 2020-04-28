91928440b28a0407d37e20a0682fecd4c6f65262
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-04-28 17:08:16 +0100

    Fix clone script

--- src/toolchain/clone.sh
+++ src/toolchain/clone.sh
@@ -51,7 +51,7 @@ if [ ! -d $DIR_NEWLIB ]; then
 fi
 
 cd $DIR_NEWLIB
-git checkout -B $BRANCH_NAME riscv-newlib-3.1.0
+git checkout -b $BRANCH_NAME
 cd -
 
 # ------ Proxy Kernel (PK) -------------------------------------------------
